53443443d29258dec554596d5e36e6c09aa445ab
Opalyimyong <khemjirayimyong@gmail.com>
2026-05-09 17:21:52 +0700

    Update game_type.h

---
+++ game_type.h
@@ -0,0 +1,14 @@
+#pragma once
+
+enum class ResourceType {
+    Coal, Gas, Biomass, Uramiun
+};
+
+enum class PlantType {
+    CoalPlant, GasPlant, BiomassPlant, SolarPlant, WindPlant, HydroPlant, NuclearPlant
+};
+
+enum class TransportType {
+    Resource, Energy
+};
+
