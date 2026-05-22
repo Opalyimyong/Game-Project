234a8222d5726abb19afd946fb96d1b98195a983
Opalyimyong <khemjirayimyong@gmail.com>
2026-05-23 03:02:38 +0700

    Update game_manage.h

--- backend/game_manage.h
+++ backend/game_manage.h
@@ -5,3 +5,19 @@
 
 #include "game_data.h"
 #include "player.h"
+#include "link.h"
+
+class GameManager {
+public:
+    GameManager();
+    void update();
+    void addPlayer(Player *player);
+    void addLink(Link *link);
+    std::vector<Player *> getPlayers() const;
+    std::vector<Link *> getLinks() const;
+
+private:
+    std::vector<Player *> players_;
+    std::vector<Node *> nodes_;
+    std::vector<Link *> links_;
+};
